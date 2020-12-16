9f7f95dcd1fae249c624c58fc66b6540727b2ad8
KaoruBB <kaorugmbl1234@gmail.com>
2020-12-16 09:45:10 +0900

    test

--- preprocess/008_number/02/python_awesome.py
+++ preprocess/008_number/02/python_awesome.py
@@ -2,6 +2,9 @@ import numpy as np
 from preprocess.load_data.data_loader import load_hotel_reserve
 customer_tb, hotel_tb, reserve_tb = load_hotel_reserve()
 
+#print(reserve_tb)
 # 下の行から本書スタート
 reserve_tb['total_price_log'] = \
   reserve_tb['total_price'].apply(lambda x: np.log(x / 1000 + 1))
+
+print(reserve_tb)
